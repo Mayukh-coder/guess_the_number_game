be20471026e6824ba81e21536eb16f6409cf1529
Mayukh <mayukhsaikia@yahoo.com>
2021-02-02 19:37:56 +0530

    Made a option for the user to enter tha game or exit

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 import random
 print('''
 ######## Welcome to the "Guess the number" Game ########
+    This is a game where the computer guesses a word and you have to guess the number.
+    Please choose an option
+        1. Enter the game (Enter '1')
+        2. Close the game (Enter '2')
 ''')
+while(True):
+    startOption=input("What is you option? ")
+    if startOption=='2':
+        exit()
+    elif startOption=='1':
+        break
+    else:
+        print("Please eneter a valid option")
+
 randomInteger=random.randint(1,101)
 print("The computer has guessed a number. Try to guess the number in minimum guesses")
 while(True):
@@ -10,4 +23,4 @@ while(True):
         print("Yaayy!! You won!")
         exit()
     else:
-        print("Oops This is not the number. Try again")
+        print("Oops This is not the number. Try again") 
