0d40fc5f3691ba2498953cde2be867ebec51e173
Mayukh <mayukhsaikia@yahoo.com>
2021-02-02 20:58:41 +0530

    Added highscore feature

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import random
+import time
 print('''
 ######## Welcome to the "Guess the number" Game ########
     This is a game where the computer guesses a word and you have to guess the number.
     Please choose an option
         1. Enter the game (Enter '1')
         2. Close the game (Enter '2')
+        3. Check the highscore(the least guesses) (Enter '3')
 ''')
 while(True):
     startOption=input("What is you option? ")
@@ -12,6 +14,13 @@ while(True):
         exit()
     elif startOption=='1':
         break
+    elif startOption=='3':
+        with open("highscore.txt") as f:
+            highscore=int(f.read())
+        if highscore==0 :
+            print("You haven't played the game yet")
+        else:
+            print(f"The number has been guessed in just {highscore} guesses. Try to break it")
     else:
         print("Please eneter a valid option")
 
@@ -22,6 +31,20 @@ while(True):
     userEnteredNumber=int(input("Enter you guess: "))
     if userEnteredNumber==randomInteger :
         print(f"Yaayy!! You won! You have won the game in {guesses} guesses. Congratulations.")
+        print("Let me check the highscores ...")
+        time.sleep(3)
+        with open("highscore.txt") as f:
+            highscore=int(f.read())
+        if highscore==0 :
+            print("Wohoo!! You have beaten the highscore")
+            with open("highscore.txt",'w') as f:
+                f.write(str(guesses))
+        elif highscore > guesses:
+            print("Wohoo!! You have beaten the highscore")
+            with open("highscore.txt",'w') as f:
+                f.write(str(guesses))
+        else:
+            print("Oops you missed in breaking the highscore")
         exit()
     else:
         print("Oops!! This is not the number.")
